f8a23ea24ea8b471b2292411626543147f9d7f64
PongHeads <sixseveneleven671178@gmail.com>
2026-08-07 19:47:47 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache name bumped every release so stale builds cannot survive an update. (build C4)
-const CACHE = 'bph-J7';
+const CACHE = 'bph-J8';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
